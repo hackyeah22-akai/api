4a5e480433e54aac69cd9b55973b837cd3abe427
siemieniuk <szymon.siemieniuk@akai.org.pl>
2022-11-20 00:05:07 +0100

    Fixed classification

--- app/clothes/utilities.py
+++ app/clothes/utilities.py
@@ -1,42 +1,42 @@
 import datetime
 
 
-# TODO: what if I wear summer t-shirt at winter?
 def is_used(cloth_date: datetime.date, c_seasons) -> bool:
     TODAY = datetime.date.today()
-    # def date_range_season(params):
-    #     R = datetime.date(*params[0])
-    #     L = datetime.date(*params[1])
-    #     if R < TODAY < L:
-    #         return (R, L)
-    #     if R > TODAY and L > TODAY:
-    #         return (
-    #             datetime.date(R.year-1, R.month, R.day),
-    #             datetime.date(L.year-1, L.month, L.day)
-    #         )
-    #     return (R, L)
+    if (interval := (TODAY-cloth_date).days) > 365:
+        return False
+    elif interval < 90:
+        return True
 
-    # ty = TODAY.year
-    # SEASON_RANGES = (
-    #     ((ty, 3, 21), (ty, 6, 21)),
-    #     ((ty, 6, 22), (ty, 9, 22)),
-    #     ((ty, 9, 23), (ty, 12, 21)),
-    #     ((ty, 12, 22), (ty+1, 3, 20))
-    # )
+    def date_range_season(params):
+        R = datetime.date(*params[0])
+        L = datetime.date(*params[1])
+        if R < TODAY < L:
+            return (R, L)
+        if R > TODAY and L > TODAY:
+            return (
+                datetime.date(R.year-1, R.month, R.day),
+                datetime.date(L.year-1, L.month, L.day)
+            )
+        return (R, L)
+
+    ty = TODAY.year
+    SEASON_RANGES = (
+        ((ty, 3, 21), (ty, 6, 21)),
+        ((ty, 6, 22), (ty, 9, 22)),
+        ((ty, 9, 23), (ty, 12, 21)),
+        ((ty, 12, 22), (ty+1, 3, 20))
+    )
 
     # spring, summer, autumm, winter: [0: 3]
-    # SEASONS = tuple(map(date_range_season, SEASON_RANGES))
+    seasons = tuple(map(date_range_season, SEASON_RANGES))
 
-    # all_days = 0
-    # for i in c_seasons:
-    #     left = SEASONS[i][0]
-    #     right = SEASONS[i][1]
-    #     if (left < cloth_date < right):
-    #         return True
-    #     else:
-    #         all_days += (right-left).days
-    # return True if all_days <= 90 else False
-
-    if (TODAY-cloth_date).days > 365:
-        return False
-    return True
+    all_days = 0
+    for i in c_seasons:
+        left = seasons[i][0]
+        right = seasons[i][1]
+        if (left < TODAY < right):
+            all_days += (TODAY-left).days
+        elif left < cloth_date:
+            all_days += (right-cloth_date).days
+    return True if all_days <= 90 else False
