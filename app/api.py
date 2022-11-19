1ef9cb97cc60a033ffc03d7236dca4e05939eaf8
siemieniuk <szymon.siemieniuk@akai.org.pl>
2022-11-19 23:34:25 +0100

    image uploading (not finished)

--- app/api.py
+++ app/api.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+import shutil
+
+from fastapi import FastAPI, File, UploadFile
 from sqlalchemy import Column, String
 from sqlalchemy.orm import Session
 
@@ -26,6 +28,24 @@ async def startup_event():
         user = User(email="test@test.com")
         db.add(user)
         db.commit()
+
+
+@app.post("/files/")
+async def create_file(file: bytes | None = File(default=None)):
+    if not file:
+        return {"message": "No file sent"}
+    else:
+        return {"file_size": len(file)}
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(image: UploadFile | None = None):
+    if not image:
+        return {"message": "No upload file sent"}
+    else:
+        with open(image.filename, "wb") as buffer:
+            shutil.copyfileobj(image.file, buffer)
+        return {"filename": image.filename}
 
 
 app.include_router(clothes_router.router, prefix="/api/v1")
