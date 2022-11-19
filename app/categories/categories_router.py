c20e5a13c27ab172ee98239cd217a5c56c3a37f2
siemieniuk <szymon.siemieniuk@akai.org.pl>
2022-11-19 23:12:51 +0100

    Tags in docs

--- app/categories/categories_router.py
+++ app/categories/categories_router.py
@@ -2,23 +2,31 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from app.config import get_db
+
 from . import categories_schemas, categories_service
 
 router = APIRouter(prefix="/category")
 
+TAGS = ['Categories']
 
-@router.post("", response_model=categories_schemas.Category)
+@router.post("",
+             response_model=categories_schemas.Category,
+             tags=TAGS)
 async def create_category(category: categories_schemas.CategoryCreate,
                           database: Session = Depends(get_db)):
     return categories_service.create_category(database, category)
 
 
-@router.get("", response_model=list[categories_schemas.Category])
+@router.get("",
+            response_model=list[categories_schemas.Category],
+            tags=TAGS)
 async def read_categories(database: Session = Depends(get_db)):
     return categories_service.get_categories(database)
 
 
-@router.get("/{category_id}", response_model=categories_schemas.Category)
+@router.get("/{category_id}",
+            response_model=categories_schemas.Category,
+            tags=TAGS)
 async def read_category(category_id: int,
                         database: Session = Depends(get_db)):
     return categories_service.get_category(category_id, database)
--- app/clothes/clothes_router.py
+++ app/clothes/clothes_router.py
@@ -2,28 +2,38 @@ from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
 from app.config import get_db
-from . import clothes_service, clothes_schemas
+
+from . import clothes_schemas, clothes_service
 
 router = APIRouter(prefix="/clothes")
 
+TAGS = ['Clothes']
 
-@router.post("", response_model=clothes_schemas.Cloth)
+@router.post("",
+             response_model=clothes_schemas.Cloth,
+             tags=TAGS)
 async def create_cloth(cloth: clothes_schemas.ClothCreate,
                        db: Session = Depends(get_db)):
     return clothes_service.create_cloth(db, cloth)
 
 
-@router.get("", response_model=list[clothes_schemas.Cloth])
+@router.get("",
+            response_model=list[clothes_schemas.Cloth],
+            tags=TAGS)
 async def read_clothes(db: Session = Depends(get_db)):
     return clothes_service.get_clothes(db)
 
 
-@router.get("/{cloth_id}", response_model=clothes_schemas.Cloth)
+@router.get("/{cloth_id}",
+            response_model=clothes_schemas.Cloth,
+            tags=TAGS)
 async def read_cloth(cloth_id: int, db: Session = Depends(get_db)):
     return clothes_service.get_cloth(cloth_id, db)
 
 
-@router.post("/use/{cloth_id}", status_code=status.HTTP_201_CREATED)
+@router.post("/use/{cloth_id}",
+             tags=TAGS,
+             status_code=status.HTTP_201_CREATED)
 async def add_cloth_use(cloth_id: int, db: Session = Depends(get_db)):
     use = clothes_schemas.UseCreate(cloth_id=cloth_id)
     clothes_service.add_use(db, use)
