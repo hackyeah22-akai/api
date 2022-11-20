d66665ad2923ac2d3dd42a376b486af7879048e3
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-11-20 05:05:22 +0100

    remove users package

--- app/api.py
+++ app/api.py
@@ -3,14 +3,12 @@ from fastapi import FastAPI
 from .categories import categories_router
 from .clothes import clothes_router
 from .config import *
-from .users import users_router
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(clothes_router.router, prefix="/api/v1")
 app.include_router(categories_router.router, prefix="/api/v1")
-app.include_router(users_router.router, prefix="/api/v1")
 
 
 @app.get("/health")
--- app/users/users_schemas.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class UserBase(BaseModel):
-    email: str
-
-    class Config:
-        orm_mode = True
